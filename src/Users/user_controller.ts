2b86b4df769135a53d7a9b0256d42fc124677863
Luc <telliezluc@gmail.com>
2025-02-05 18:30:17 +0100

    User fonctionnel

--- src/Users/user_controller.ts
+++ src/Users/user_controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import prisma from '../client';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { re } from 'mathjs';
 
 // Fonction pour créer un utilisateur
 export const createUser = async (req: Request, res: Response) => {
@@ -22,6 +24,7 @@ export const createUser = async (req: Request, res: Response) => {
 
     if (existingEmail) {
         res.status(400).send({ error: 'Email déjà utilisé' });
+        return;
     }
 
     const user = await prisma.user.create({
@@ -32,4 +35,43 @@ export const createUser = async (req: Request, res: Response) => {
     });
 
     res.status(201).send(user);
+}
+
+// Fonction pour se connecter
+export const loginUser = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+        res.status(400).send(`Le champ 'email' est vide`);
+        return;
+    }
+
+    if (!password) {
+        res.status(400).send(`Le champ 'password' est vide`);
+        return;
+    }
+
+    const user = await prisma.user.findUnique({
+        where: { email: email }
+    });
+
+    if (!user) {
+        res.status(400).send({ error: `L'utilisateur n'existe pas` });
+        return;
+    }
+
+    const testPassword = await bcrypt.compare(password, user.password);
+    if (!testPassword) {
+        res.status(400).send({ error: 'Mot de passe incorrect' });
+        return;
+    }
+
+    const token = jwt.sign(
+        { id: user.id, email: user.email },
+        process.env.JWT_SECRET as string,
+        { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.status(201).send({ token });
+    
 }
--- src/Users/user_router.ts
+++ src/Users/user_router.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
-import { createUser } from './user_controller';
+import { createUser, loginUser } from './user_controller';
  
 export const user_router = Router();
 
 // Route pour créer un utilisateur
 user_router.post('/', createUser)
+
+// Router pour se connecter
+user_router.post('/login', loginUser)
